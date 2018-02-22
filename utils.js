d207a7cb561eff83bd74aaa4f579f5c5019a0557
Yann Stepienik <azukaar@gmail.com>
2018-02-22 10:02:01 +0000

    fix

--- utils.js
+++ utils.js
@@ -1,19 +1,18 @@
+const Poloniex = require('./exchanges/poloniex');
+const poloniex = new Poloniex();
+
 module.exports ={
     convertToUSDIfPossible(value) {
-        if(['ETH', 'BTC'].indexOf(value.currency) > -1) {
-            const Poloniex = require('./exchanges/poloniex');
-            const poloniex = new Poloniex();
+        if(['ETH', 'LTC', 'BTC'].indexOf(value.currency) > -1) {
             if(value.timestamp) {
                 return poloniex.tickAtTimestamp(value.currency+'_USD', parseInt(value.timestamp)).then(tick => {
                     value.value = value.value * tick[0].high;
                     value.currency = 'USD';
-                    return value
+                    return value;
                 });
             }
             else {
-                const Bitfinex = require('./exchanges/bitfinex');
-                const bitfinex = new Bitfinex();
-                return bitfinex.tick(value.currency+'_USD').then( tick => {
+                return poloniex.tick(value.currency+'_USD').then( tick => {
                     value.value = value.value * tick.last;
                     value.currency = 'USD';
                     return value;
